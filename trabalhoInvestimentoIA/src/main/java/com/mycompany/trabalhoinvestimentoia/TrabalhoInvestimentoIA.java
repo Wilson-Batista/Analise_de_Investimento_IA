0b0e4abc8ab3ca476aefdb0468632a6be48ce1d5
wilson <wilsonescorpiao7@gmail.com>
2023-07-08 17:21:56 -0300

    atualização

---
+++ trabalhoInvestimentoIA/src/main/java/com/mycompany/trabalhoinvestimentoia/TrabalhoInvestimentoIA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalhoinvestimentoia;
+
+/**
+ *
+ * @author wilson
+ */
+public class TrabalhoInvestimentoIA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
